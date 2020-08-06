4fb69a06ff9bcce7d254c3868f30c9bf22dbc409
LacerdaAlexandre <68246198+LacerdaAlexandre@users.noreply.github.com>
2020-08-05 22:03:05 -0300

    Dia 3
    Finalizada a parte de CSS e iniciada a parde de Ecma

--- scripts/addField.js
+++ scripts/addField.js
@@ -1,9 +1,25 @@
-//Procurar o botão
-document.querySelector
-//Quando clicar no botão
+document.querySelector("#add-time") //Procurar o botão
+.addEventListener('click',cloneField) //Quando clicar no botão
+
 
 //Executar uma ação
+function cloneField(){
+    //Duplicar os campos. Que campos?
+    const newFieldContainer = document.querySelector('.schedule-item')
+    .cloneNode(true)
 
-    //Duplicar os campos
+    //Limpar os campos. Que campos?
+    const fields = newFieldContainer.querySelectorAll('input')
 
-    //Colocar na página
+    //para campo limpa
+
+    fields.forEach(function(field) {
+        //pegar o field do momento e limpa
+        field.value = ""
+
+    });
+
+    //Colocar na página onde?
+    document.querySelector('#schedule-items')
+    .appendChild(newFieldContainer)
+}
